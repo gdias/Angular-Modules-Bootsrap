a39b84bb0f3b023d37be86f0a5713e31a5b31c80
dvilston <dvilston@gmail.com>
2016-06-11 13:26:18 +0200

    Remove old files and minor fix

--- app/shared/menu/menuController.js
+++ app/shared/menu/menuController.js
@@ -2,8 +2,6 @@
 
 module.exports.menuController = ['$scope', '$http', '$rootScope', function menuController ($scope, $http, $rootScope){
 
-  console.log("menuController - ", $scope);
-
   $scope.message = "Menu items"
   $scope.tpl = {}
   $scope.tpl.contentUrl = 'partials/commons/menu.html'
--- public/js/main.js
+++ public/js/main.js
@@ -806,8 +806,6 @@ module.exports.config = [
 
 module.exports.menuController = ['$scope', '$http', '$rootScope', function menuController ($scope, $http, $rootScope){
 
-  console.log("menuController - ", $scope);
-
   $scope.message = "Menu items"
   $scope.tpl = {}
   $scope.tpl.contentUrl = 'partials/commons/menu.html'
